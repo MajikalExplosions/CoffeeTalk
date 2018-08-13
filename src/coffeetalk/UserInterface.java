0d3aab00f6ae2d3ce1c62b3cac1c1035b0c90084
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2018-08-13 14:46:01 -0700

    Fixed!

--- src/coffeetalk/UserInterface.java
+++ src/coffeetalk/UserInterface.java
@@ -13,19 +13,57 @@ public class UserInterface  {
 	
 	JFrame window;
 	DataStore data;
+	JPanel content;
+	JPanel header;
+	JPanel footer;
 	
 	public void InitialiseWindow() {
 		
+		data = new DataStore();
+		
 		//Create JFrame
 		window = new JFrame("CoffeeTalk");
+		content = new JPanel();
+		header = new JPanel();
+		footer = new JPanel();
 		
 		//I don't like background programs.
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        startPage(Page.Start);
+        window.setSize(200, 100);
+		window.setLayout(new java.awt.GridLayout(2, 1));
+		
+		JButton submit = new JButton("Start");
+		JPanel input = new JPanel();
+		input.setLayout(new java.awt.GridLayout(1, 2));
+		input.add(new JLabel("Username"));
+		JTextField name = new JTextField();
+		input.add(name);
+		
+		submit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	if (! name.getText().equals("")) {
+            		data.username = name.getText();
+            		
+            		//Add stuff
+            		window.getContentPane().removeAll();
+            		window.setLayout(new java.awt.BorderLayout());
+            		window.add(content, java.awt.BorderLayout.CENTER);
+            		window.add(header, java.awt.BorderLayout.NORTH);
+            		window.add(footer, java.awt.BorderLayout.SOUTH);
+            		
+            		startPage(Page.Encoder);
+            	}
+            }
+        });
+		
+		window.add(input);
+		window.add(submit);
+		window.setVisible(true);
 	}
 	
 	public void startPage(Page p) {
+		content.removeAll();
 		
 		switch(p) {
 		case Encoder:
@@ -37,34 +75,12 @@ public class UserInterface  {
 		case Settings:
 			//Settings Page
 			break;
-		case Start:
-			//Start Page
-			window.setSize(200, 100);
-			window.setLayout(new java.awt.GridLayout(2, 1));
-			
-			JButton submit = new JButton("Start");
-			JPanel input = new JPanel();
-			input.setLayout(new java.awt.GridLayout(1, 2));
-			input.add(new JLabel("Username"));
-			JTextField name = new JTextField();
-			input.add(name);
-			
-			submit.addActionListener(new ActionListener() {
-	            public void actionPerformed(ActionEvent e) {
-	            	if (! name.getText().equals("")) {
-	            		data.username = name.getText();
-	            		startPage(Page.Encoder);
-	            	}
-	            }
-	        });
-			
-			window.add(input);
-			window.add(submit);
-			break;
 		default:
 			//the heck why's it here
 			break;
 		}
+		window.validate();
+		window.repaint();
 		window.setVisible(true);
 	}
 	
