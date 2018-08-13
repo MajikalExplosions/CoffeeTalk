19fd8d252335aef49b62fcad96a63f5a1fb0b074
Jack <jackbchen929@gmail.com>
2018-08-13 14:14:56 -0700

    defined presets for caesar cipher encryption

--- src/coffeetalk/DataStore.java
+++ src/coffeetalk/DataStore.java
@@ -4,6 +4,13 @@ public class DataStore {
 	public String username;
 	public Theme[] themes;
 	public int port;
+	public final int[] presetKeys = {
+		34,	
+		-12,	
+		68,
+		44,
+		-56
+	};
 	
 	
 }
