1af5a537981fc8b6c2e811ef0b9d4b23c2b5009f
Jack <jackbchen929@gmail.com>
2018-08-13 11:22:07 -0700

    New Class

---
+++ src/coffeetalk/UserInterface.java
@@ -0,0 +1,14 @@
+package coffeetalk;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+public class UserInterface {
+
+	
+	
+	
+	
+}
