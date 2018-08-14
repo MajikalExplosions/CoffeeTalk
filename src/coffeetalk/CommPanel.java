1e95a139ff7721b082be6565ddb4daa14433e4db
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2018-08-14 12:03:29 -0700

    Whoops 2

--- src/coffeetalk/CommPanel.java
+++ src/coffeetalk/CommPanel.java
@@ -189,7 +189,7 @@ public class CommPanel {
 	                chat.displayChatMessage(new ChatMessage("[Server]", "Connected!"));
 	                break;
 	            }
-	            catch(NumberFormatException | NoRouteToHostException | UnknownHostException e2) {
+	            catch(NumberFormatException | NoRouteToHostException | StringIndexOutOfBoundsException | UnknownHostException e2) {
 	            	chat.displayChatMessage(new ChatMessage("[Server]", "IP Invalid."));
 	            	i2 += 4;
 	            	
